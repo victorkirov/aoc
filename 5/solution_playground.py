30c9617f9725f96a010bca59ac4d4c05a1b3c17f
victorkirov <victor.kirov@gmail.com>
2020-12-05 08:13:50 +0200

    Made playground dry

--- 5/solution_playground.py
+++ 5/solution_playground.py
@@ -1,36 +1,16 @@
 lines = [l.strip() for l in open('input') if l]
 values = [v.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1') for v in lines]
+ids = [int(value[:7], 2) * 8 + int(value[7:], 2) for value in values]
+ids.sort()
 
 
 def part1():
-    min = None
-    max = None
-
-    for value in values:
-        row = int(value[:7], 2)
-        column = int(value[7:], 2)
-
-        id = row * 8 + column
-        max = id if max is None or max < id else max
-        min = id if min is None or min > id else min
-
-    return min, max
+    return max(ids)
 
 
 def part2():
-    min, max = part1()
-    all_seats = list(range(min, max + 1))
-
-    for value in values:
-        row = int(value[:7], 2)
-        column = int(value[7:], 2)
-
-        id = row * 8 + column
-        all_seats.remove(id)
-
-    return all_seats[0]
+    return next(iter(set(range(ids[0], ids[-1])).difference(set(ids))))
 
 
-
-print('Part 1: ', part1()[1])
+print('Part 1: ', part1())
 print('Part 2: ', part2())
