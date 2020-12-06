08f77a99e24521d1f84f29a749a6e486e7e1a692
victorkirov <victor.kirov@gmail.com>
2020-12-06 07:12:39 +0200

    Day 6

---
+++ 6/solution.py
@@ -0,0 +1,43 @@
+lines = [l.strip() for l in open('input') if l]
+values = [v for v in lines]
+
+
+def part1():
+    group = set()
+    group_items = []
+
+    for value in values:
+        group.update(set(list(value)))
+
+        if not value:
+            group_items.append(len(group))
+            group = set()
+
+    group_items.append(len(group))
+
+    print(group_items)
+
+    return sum(group_items)
+
+
+def part2():
+    group = None
+    group_items = []
+
+    for value in values:
+        if group is None:
+            group = set(list(value))
+        elif value:
+            group.intersection_update(set(list(value)))
+
+        if not value:
+            group_items.append(len(group))
+            group = None
+
+    group_items.append(len(group))
+
+    return sum(group_items)
+
+
+print('Part 1: ', part1())
+print('Part 2: ', part2())
