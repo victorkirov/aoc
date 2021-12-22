e703c475e0166bdcf0bead5975c78910da204de3
Vic <victor@nomanini.com>
2021-12-22 11:16:08 +0200

    Day 22 part 2

--- 2021/22/solution.py
+++ 2021/22/solution.py
@@ -1,9 +1,10 @@
 from functools import reduce, cache
 from collections import defaultdict
 from itertools import product, combinations, permutations
+from typing import List
 
 # strings
-lines = [l.strip() for l in open('input2') if l]
+lines = [l.strip() for l in open('input') if l]
 values = [v for v in lines]
 
 commands = []
@@ -49,9 +50,103 @@ def part1():
     return len(on_set)
 
 
+def process_diff(
+    region,
+    x_start,
+    x_end,
+    y_start,
+    y_end,
+    z_start,
+    z_end
+) -> List:
+    [
+        (rx_start, ry_start, rz_start),
+        (rx_end, ry_end, rz_end),
+    ] = region
+
+    x_vals = [x_start, x_start - 1, x_end, x_end + 1, rx_start, rx_end]
+    y_vals = [y_start, y_start - 1, y_end, y_end + 1, ry_start, ry_end]
+    z_vals = [z_start, z_start - 1, z_end, z_end + 1, rz_start, rz_end]
+
+    x_vals.sort()
+    y_vals.sort()
+    z_vals.sort()
+
+    regions = []
+
+    for xi in range(3):
+        xl = x_vals[xi * 2]
+        xr = x_vals[xi * 2 + 1]
+        for yi in range(3):
+            yl = y_vals[yi * 2]
+            yr = y_vals[yi * 2 + 1]
+            for zi in range(3):
+                zl = z_vals[zi * 2]
+                zr = z_vals[zi * 2 + 1]
+
+                if (
+                    rx_start <= xl <= rx_end
+                    and ry_start <= yl <= ry_end
+                    and rz_start <= zl <= rz_end
+                    and rx_start <= xr <= rx_end
+                    and ry_start <= yr <= ry_end
+                    and rz_start <= zr <= rz_end
+                    and not (
+                        x_start <= xl <= x_end
+                        and y_start <= yl <= y_end
+                        and z_start <= zl <= z_end
+                        and x_start <= xr <= x_end
+                        and y_start <= yr <= y_end
+                        and z_start <= zr <= z_end
+                    )
+                ):
+                    regions.append(
+                        [
+                            (xl, yl, zl),
+                            (xr, yr, zr),
+                        ]
+                    )
+
+    return regions
+
+
+
 def part2():
-    # 2758514936282235
-    pass
+    regions = []
+    for (
+        command,
+        x_start,
+        x_end,
+        y_start,
+        y_end,
+        z_start,
+        z_end
+    ) in commands:
+        new_regions = []
+        for region in regions:
+            new_regions += process_diff(
+                region,
+                x_start,
+                x_end,
+                y_start,
+                y_end,
+                z_start,
+                z_end
+            )
+        if command == 'on':
+            new_regions.append(
+                [
+                    (x_start, y_start, z_start),
+                    (x_end, y_end, z_end),
+                ]
+            )
+        regions = new_regions
+
+    return sum([
+        (end[0] - start[0] + 1) * (end[1] - start[1] + 1) * (end[2] - start[2] + 1)
+        for [start, end]
+        in regions
+    ])
 
 
 print('Part 1: ', part1())
