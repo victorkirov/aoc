43d4c4c4f06787e5aaab4843013d71ac05cc52b6
Victor <victor@nomanini.com>
2022-12-01 11:17:55 +0200

    Fix small solution day 1 2022

--- 2022/1/solution_small.py
+++ 2022/1/solution_small.py
@@ -1,3 +1,3 @@
 (values := [sum([int(i) for i in v.strip().split('\n')]) for v in open('input').read().split('\n\n')]).sort(reverse=True)
-print('Part1:', max(values))
+print('Part1:', values[0])
 print('Part2:', sum(values[:3]))
